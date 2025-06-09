97f63963ee074b6f63bbe604a0049db58c853cb7
j.skorżański_Desktop <kubaskorzanski@gmail.com>
2025-06-09 23:12:06 +0200

    init commit

--- src/main/java/com/example/courierservice/mas_projekt/GUI/Logowanie.java
+++
@@ -1,4 +0,0 @@
-package com.example.courierservice.mas_projekt.GUI;
-
-public class Logowanie {
-}
---
+++ src/main/java/com/example/courierservice/mas_projekt/Logowanie.java
@@ -0,0 +1,11 @@
+package com.example.courierservice.mas_projekt;
+
+public class Logowanie {
+    private String login;
+    private String haslo;
+
+    public Logowanie(String login, String haslo) {
+        this.login = login;
+        this.haslo = haslo;
+    }
+}
